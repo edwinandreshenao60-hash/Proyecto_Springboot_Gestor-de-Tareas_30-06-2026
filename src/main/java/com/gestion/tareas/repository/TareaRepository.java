5ea8d5fb3ff07bcbe160058af957d3212ba55c55
Edwin Henao <edwinandreshenao60@gmail.com>
2026-06-30 15:36:22 -0500

    Se expande TareaRepository con metodos de filtrado, busqueda y ordenamiento sin comentarios en el codigo

--- src/main/java/com/gestion/tareas/repository/TareaRepository.java
+++ src/main/java/com/gestion/tareas/repository/TareaRepository.java
@@ -1,11 +1,21 @@
 package com.gestion.tareas.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.gestion.tareas.entity.Tarea;
+import com.gestion.tareas.entity.EstadoTarea;
+import com.gestion.tareas.entity.PrioridadTarea;
 import java.util.List;
 
 @Repository
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
-    List<Tarea> findByActivoTrue();
+    
+    List<Tarea> findByActivoTrue(Sort sort);
+    
+    List<Tarea> findByEstadoAndActivoTrue(EstadoTarea estado);
+    
+    List<Tarea> findByPrioridadAndActivoTrue(PrioridadTarea prioridad);
+    
+    List<Tarea> findByTituloContainingIgnoreCaseAndActivoTrue(String titulo);
 }
